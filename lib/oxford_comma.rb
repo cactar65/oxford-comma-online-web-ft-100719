96d46086ecf63be2f1a1b2a3c3601e7d5a22d65f
Nicholas Walton <cactar65@gmail.com>
2019-10-12 22:35:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.length <= 2
+    joined = array.join(" and ")
+  return joined
+  
+  else array.length >= 3
+    
+    return (array.first array.size-1).join(", ") + ", and #{array.last}"
+    
+  end
+  
 end
